6729f2bf46cf99101abf4535c4d3a1fa2943fb88
GulTion <gkv8192@gmail.com>
2020-05-23 00:40:21 -0700

    Updates

---
+++ precache-manifest.8afcc3684c8f9a5ba7940f310786f2e2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d9e018f3401e07657fa340fff1b9f7",
+    "url": "/GOS/index.html"
+  },
+  {
+    "revision": "d4c94dfca1b0f6e168d3",
+    "url": "/GOS/static/css/main.4b710c51.chunk.css"
+  },
+  {
+    "revision": "9ff1d325ffc8717517db",
+    "url": "/GOS/static/js/2.8129b9ca.chunk.js"
+  },
+  {
+    "revision": "d4c94dfca1b0f6e168d3",
+    "url": "/GOS/static/js/main.de05324f.chunk.js"
+  },
+  {
+    "revision": "46136867566d4253c826",
+    "url": "/GOS/static/js/runtime-main.2eb93702.js"
+  },
+  {
+    "revision": "9e158fdf03a4f8c4448ca2b718a380ca",
+    "url": "/GOS/static/media/resize.9e158fdf.ico"
+  },
+  {
+    "revision": "8dc57db516c767e59517b3bb72a66ea3",
+    "url": "/GOS/static/media/wall.8dc57db5.png"
+  }
+]);
